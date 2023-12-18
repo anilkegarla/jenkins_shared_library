d4336d55ea7d718e41fdd3888f290958295a92ef
Anilkumar <anilkegarla@gmail.com>
2023-12-18 13:29:59 +0530

    files updated

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,5 @@
 def call(Map stageParams) {
 
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
+checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/anilkegarla/myrepo1.git']])
+    
   }
